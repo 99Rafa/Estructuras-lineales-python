21050f8f161de0916ee25e3b751c68d72b9f9b45
Rafael Aguirre <rafael.aguirre.ro@gmail.com>
2021-08-07 01:09:03 -0600

    Queue basada en dos stacks

---
+++ stack_based_queue.py
@@ -0,0 +1,16 @@
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.inbound_stack = []
+        self.outbound_stack = []
+
+    def enqueue(self, data) -> None:
+        self.inbound_stack.append(data)
+
+    def dequeue(self) -> any:
+        if not self.outbound_stack:
+            while self.inbound_stack:
+                self.outbound_stack.append(self.inbound_stack.pop())
+
+        return self.outbound_stack.pop()
